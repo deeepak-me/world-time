fca36b2023df1a3de0d295a28d3933df9db746d0
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-21 02:01:41 +0530

    feat: added app bar and title

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,9 +8,21 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location',
+        style: TextStyle(
+          color: Colors.white
+        ),),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Text('Choose Liocation screen'),
     );
   }
