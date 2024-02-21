f8afa8aa60afcf652693a55e7a532261abbd61d5
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-21 16:34:03 +0530

    feat: time formated using intl package

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String?location; //location name for the UI
@@ -30,7 +31,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
 
-      time = now.toString();//set the time property
+      time = DateFormat.jm().format(now);//set the time property
 
     } catch (e) {
       print('Error: $e');
